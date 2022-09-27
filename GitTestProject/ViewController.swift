b8faa448e212fdc8dcefaa8f50f26e59ad9b3999
Onur Kösemen <onur_kosemen@hotmail.com>
2022-09-27 10:34:23 +0300

    first commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Onur Kösemen on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("git")
+    }
+
+
+}
+
